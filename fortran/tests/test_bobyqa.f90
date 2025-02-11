fa57d873434ca3f445855af714e401ee6e243270
Pranavchiku <goswami.4@iitj.ac.in>
2025-02-11 21:00:30 +0530

    XX: nullify()

--- fortran/tests/test_bobyqa.f90
+++ fortran/tests/test_bobyqa.f90
@@ -192,7 +192,7 @@ if (testdim_loc == 'big' .or. testdim_loc == 'large') then
             & ftarget=ftarget, iprint=iprint)
 
         deallocate (x)
-        nullify (orig_calfun)
+        ! nullify (orig_calfun)
     end do
     ! DESTRUCT deallocates allocated arrays/pointers and nullify the pointers. Must be called.
     call destruct(prob)  ! Destruct the testing problem.
@@ -276,7 +276,7 @@ else
                 end if
 
                 deallocate (x)
-                nullify (orig_calfun)
+                ! nullify (orig_calfun)
             end do
 
             ! DESTRUCT deallocates allocated arrays/pointers and nullify the pointers. Must be called.

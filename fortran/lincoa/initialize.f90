5ae46e0fe16ffcb67b1dd440fdda3b53b64485e4
Ondřej Čertík <ondrej@certik.us>
2025-02-23 16:17:09 -0700

    Remove

--- fortran/lincoa/initialize.f90
+++ fortran/lincoa/initialize.f90
@@ -105,17 +105,11 @@ integer(IK) :: subinfo
 integer(IK), allocatable :: ixl(:)
 integer(IK), allocatable :: ixu(:)
 logical :: feasible(size(xpt, 2))
-! @@@@@@@----------------------------------------> ////// WORKAROUND ///// <----------------------------------------@@@@@@@@
-real(RP) ,allocatable:: constr(:) 
-
+real(RP) :: constr(count(xl > -BOUNDMAX) + count(xu < BOUNDMAX) + 2 * size(beq) + size(bineq))
 real(RP) :: constr_leq(size(beq))
 real(RP) :: cstrv
 real(RP) :: f
 real(RP) :: x(size(x0))
-
-
-! @@@@@@@----------------------------------------> ////// WORKAROUND ///// <----------------------------------------@@@@@@@@
-allocate(constr(count(xl > -BOUNDMAX) + count(xu < BOUNDMAX) + 2 * size(beq) + size(bineq)))
 
 ! Sizes.
 m = int(size(b), kind(m))
